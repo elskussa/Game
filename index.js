0fede575acc3b7d7bad71c1dc3e5758a5ed55508
elskussa <111130416+elskussa@users.noreply.github.com>
2025-11-06 01:19:09 +0100

    index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const subventana = document.querySelectorAll('.subventana');
 const botonReset = document.querySelector('.botonReset');
 const marcoAvisos = document.querySelector('.avisos');
 const ventanas = document.querySelector('.ventanas')
+const textoImagenDesonrra = document.querySelector('.textoImagenDesonrra');
+const imagenDesonrraNinho = document.querySelector('.imagenDesonrraNinho');
+const imagenDesonrra = document.querySelector('.imagenDesonrra');
 
 let numeroDeMuertes = 0;
 
@@ -18,10 +21,7 @@ function moverMunheco() {
 };
 
 function revivirMunheco() {
-    const nuevoMunheco = document.createElement('div');
-    nuevoMunheco.className = 'Munheco';
     ventanas.appendChild(munheco);
-    nuevoMunheco.onclick = moverMunheco;
     marcoAvisos.innerHTML = "";
 }
 
@@ -32,6 +32,19 @@ subventana.forEach((element, i) => {
             botonReset.style.color = 'white';
             munheco.remove()
             numeroDeMuertes++;
+            if(numeroDeMuertes == 3) {
+                textoImagenDesonrra.innerHTML = `has muerto ${numeroDeMuertes} veces, eres un mierdas`
+                const imagenNinho = document.createElement('img');
+                imagenNinho.className = 'img';
+                imagenNinho.src = './fotos/imagenNinho.png'
+                imagenDesonrraNinho.appendChild(imagenNinho);
+                imagenDesonrra.style.display = 'flex';
+                if(!imagenNinho.dataset.eventListenerAdded) {
+                    imagenNinho.addEventListener('click', () => {
+                        imagenDesonrra.style.display = 'none';
+                    })
+                }
+            }
             marcoAvisos.innerHTML = `Perdiste ${numeroDeMuertes} veces`;
             console.log(`has muerto ${numeroDeMuertes}`)
         });
